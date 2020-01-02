63893a03d98c481fadceb43cd896721d8c3436af
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-01-02 15:35:59 +0200

    Implement string concat function

--- app/Smart.php
+++ app/Smart.php
@@ -21,8 +21,27 @@ class Smart
             : $default;
     }
 
+    /**
+     * Arrays can be concatenated also
+     *
+     * @param array $first
+     * @param array $second
+     * @return array
+     */
     public static function arrConcat(array $first, array $second)
     {
         return $first + $second;
+    }
+
+    /**
+     * Let's make String concatenation operator great again
+     *
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
+    public static function strConcat(string $first, string $second)
+    {
+        return $first . $second;
     }
 }
